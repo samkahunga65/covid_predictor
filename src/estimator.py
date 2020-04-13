481b12364c8e384935175c3e53d49a80f8db7608
ohongs <samkahunga65@gmail.com>
2020-04-13 16:36:27 +0300

    second commit of the first challenge

--- src/estimator.py
+++ src/estimator.py
@@ -1,17 +1,3 @@
-covid = {
-    'region': {
-        'name': "Africa",
-        'avgAge': 19.7,
-        'avgDailyIncomeInUSD': 5,
-        'avgDailyIncomePopulation': 0.71
-    },
-    'periodType': "days",
-    'timeToElapse': 58,
-    'reportedCases': 674,
-    'population': 66622705,
-    'totalHospitalBeds': 1380614
-}
-
 
 def estimator(data):
     o_data = data
@@ -24,8 +10,4 @@ def estimator(data):
         'data': o_data,
         'impact': {'currentlyInfected': i_ci, 'infectionsByRequestedTime': i_ibrt, },
         'severeImpact': {'currentlyInfected': s_ci, 'infectionsByRequestedTime': s_ibrt, }}
-    print(data)
     return data
-
-
-estimator(covid)
