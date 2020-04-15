29623747292c7167a31b16c802a6f2e11add3e14
ohongs <samkahunga65@gmail.com>
2020-04-15 20:25:19 +0300

    sdscs

--- src/estimator.py
+++ src/estimator.py
@@ -1,17 +1,17 @@
 import math
-covid = {
-    'region': {
-        'name': "Africa",
-        'avgAge': 19.7,
-        'avgDailyIncomeInUSD': 5,
-        'avgDailyIncomePopulation': 0.71
-    },
-    'periodType': "month",
-    'timeToElapse': 2,
-    'reportedCases': 674,
-    'population': 66622705,
-    'totalHospitalBeds': 1380614
-}
+# covid = {
+#     'region': {
+#         'name': "Africa",
+#         'avgAge': 19.7,
+#         'avgDailyIncomeInUSD': 5,
+#         'avgDailyIncomePopulation': 0.71
+#     },
+#     'periodType': "weeks",
+#     'timeToElapse': 3,
+#     'reportedCases': 1,
+#     'population': 66622705,
+#     'totalHospitalBeds': 1380614
+# }
 
 
 def estimator(data):
@@ -28,17 +28,14 @@ def estimator(data):
         i_ibrt = math.trunc(i_ci*2**(data["timeToElapse"]/3))
     elif data['periodType'] == 'weeks':
         v = data["timeToElapse"] * 7
-        print(v)
         s_ibrt = math.trunc(s_ci*2**(v/3))
         i_ibrt = math.trunc(i_ci*2**(v/3))
     elif data['periodType'] == 'months':
         qwe = data["timeToElapse"] * 30
-        print(qwe)
         s_ibrt = math.trunc(s_ci*2**(qwe/3))
         i_ibrt = math.trunc(i_ci*2**(qwe/3))
     else:
         w = data["timeToElapse"] * 0
-        print(w)
         s_ibrt = math.trunc(s_ci*2**w)
         i_ibrt = math.trunc(i_ci*2**w)
     i_scbrt = math.trunc(i_ibrt*1.15)
@@ -67,7 +64,21 @@ def estimator(data):
                          'casesForVentilatorsByRequestedTime': s_rep,
                          'dollarsInFlight': s_dif}}
     # print(data)
+    # data = {
+    #     'data': o_data,
+    #     'impact': [{'currentlyInfected': i_ci}, {'infectionsByRequestedTime': i_ibrt},
+    #                {'severeCasesByRequestedTime': i_scbrt}, {
+    #                    'hospitalBedsByRequestedTime': i_hbrt},
+    #                {'casesForICUByRequestedTime': i_icu},
+    #                {'casesForVentilatorsByRequestedTime': i_rep},
+    #                {'dollarsInFlight': i_dif}],
+    #     "severeImpact": [{'currentlyInfected': s_ci}, {'infectionsByRequestedTime': s_ibrt},
+    #                      {'severeCasesByRequestedTime': s_scbrt}, {
+    #                          'hospitalBedsByRequestedTime': s_hbrt},
+    #                      {'casesForICUByRequestedTime': s_icu},
+    #                      {'casesForVentilatorsByRequestedTime': s_rep},
+    #                      {'dollarsInFlight': s_dif}]}
     return data
 
 
-print(estimator(covid))
+# print(estimator(covid)['severeImpact']['casesForICUByRequestedTime'])
